5e07bffa8063a0f0ce3ca267b91f794ae38f5056
Ashwin Khode <ashwin.khode1@gmail.com>
2020-12-13 23:36:18 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import {emojiObject} from './emoji'
+
+const emojiArray = Object.keys(emojiObject)
 
 function App() {
+  const [meaning, setMeaning] = useState("")
+
+  function inputHandler(event) {
+    const userInput = event.target.value
+    let meaning = emojiObject[userInput]
+
+    if (meaning === undefined) {
+      meaning = 'We dont have this emoji in our database'
+    }
+    setMeaning(meaning)
+  }
+
+  function emojiClickHandler(emoji) {
+    //console.log(emoji.target.innerHTML)
+    const clickEmoji = emoji.target.innerHTML
+    let meaning = emojiObject[clickEmoji]
+    setMeaning(meaning)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Emoji Dictionary</h1>
+      <input type="text" id='userInput' placeholder="Please type or click on the emoji to know its meaning" onChange={inputHandler} />
+      <div className="display">{meaning ? meaning : 'Type or Click an emoji given below'}</div>
+      <div className="emojis">
+        <span className='emojiHead'>Top 25 emojis used across the world</span><br />
+        {
+          emojiArray.map((item) =>
+            <span className='emojiList' key={item} onClick={emojiClickHandler}>{item}</span>
+          )
+        }
+      </div>
+      <div className="footer">Check out source code at <a href="http://github.com/ashwinkhode">Github</a></div>
     </div>
   );
 }
